0fa4b3807b99992027e84482e04231acbf283541
Vincent munywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-09-08 12:14:02 +0300

    Create 001-Introduction.js

---
+++ 001-Introduction.js
@@ -0,0 +1,19 @@
+Javascript is a dynamic computer programming language. It is mostly used as a part of web pages, whose implementations allow
+client-side script to interact with the user and make dynamic pages.Client-side javascript is the most common form of the language. The script
+should be included in or referenced by an HTML document for the code to be
+interpreted by the browser.
+
+You might use js to check whether the user has entered a valid e-mail address in a form fied.The JS code is executed when the user submits the form,
+and only if all the entries are valid,they would bbe submitted to the web server. Javascript can be used to trap user-initiated events such as button clicks,link navigation and
+other actions that the user initiates explicity or implicity.
+
+ADVANTAGES OF JS.
+Less sever interaction- this is done by validating the user before sending the page off to the server.this saves server traffic.
+Immediate feedback to the visitor-They dont have to wait for a page to reload.
+increased interactivity.
+Richer interfaces-I tems such as drag and drop components and sliders are included using JS to give a rich interface 
+to your site visitors.
+
+LIMITATIONS 
+client-side js does dont allow the reading and writing of files.this has been kept for security purposes.
+JS cannot be used for networking apps because it does not support such.
