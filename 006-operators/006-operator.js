bbdfb31860cbd0ec49ee6a803d560006110569b3
Vincent munywoki <87576661+vincentMunywoki@users.noreply.github.com>
2022-09-22 09:26:09 +0300

    Create 006-operator.js

---
+++ 006-operators/006-operator.js
@@ -0,0 +1,88 @@
+//JS supports - Arithmetic,comparisonlogocal/relational,assignment and conditional operatoes.
+//code showing how to use arithmetic operators in js
+<html>
+<body>
+<script type = "text/javascript">
+<!-- 
+var a = 33;
+var b = 10;
+var c = "test";
+var linebreak = "<br />";
+
+document.write("a+b = ");
+result = a+ b;
+document.write(result);
+document.write(linebreak);
+document.write("a - b = ");
+result = a - b;
+document.write(result);
+document.write(linebreak);
+document.write("a / b = ");
+result = a / b;
+document.write(result);
+document.write(linebreak);
+document.write("a % b = ");
+result = a % b;
+document.write(result);
+document.write(linebreak);
+document.write("a + b + c = ");
+result = a + b + c;
+document.write(result);
+document.write(linebreak);
+a = a++;
+document.write("a++ = ");
+result = a++;
+document.write(result);
+document.write(linebreak);
+b = b--;
+document.write("b-- = ");
+result = b--;
+document.write(result);
+document.write(linebreak);
+22Javascript
+//-->
+</script>
+<p>Set the variables to different values and then try...</p>
+</body>
+</html> 
+
+//code to show how to use comparison opertors in js it displays true or false
+
+<html>
+<body>
+<script type="text/javascript">
+<!--
+var a = 10;
+var b = 20;
+var linebreak = "<br />";
+document.write("(a == b) => ");
+result = (a == b);
+document.write(result);
+document.write(linebreak);
+24Javascript
+document.write("(a < b) => ");
+result = (a < b);
+document.write(result);
+document.write(linebreak);
+document.write("(a > b) => ");
+result = (a > b);
+document.write(result);
+document.write(linebreak);
+document.write("(a != b) => ");
+result = (a != b);
+document.write(result);
+document.write(linebreak);
+document.write("(a >= b) => ");
+result = (a >= b);
+document.write(result);
+document.write(linebreak);
+document.write("(a <= b) => ");
+result = (a <= b);
+document.write(result);
+document.write(linebreak);
+//-->
+</script>
+<p>Set the variables to different values and different operators and
+then try...</p>
+</body>
+</html>
